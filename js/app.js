ce06fe1d8178c8bfbac720fd73fa2cd738cb8b53
Jan Gabriel <johnymachine@gmail.com>
2015-03-12 01:01:42 +0100

    fix

--- js/app.js
+++ js/app.js
@@ -1,24 +1,12 @@
 var vibrate = function vibrate(){
 	'use strict'
 
-	var vibrateInterval;
-	// Starts vibration at passed in level
 	var startVibrate = function startVibrate(duration) {
 		navigator.vibrate(duration);
 	}
 
-	// Stops vibration
-	var stopVibrate = function stopVibrate() {
-	// Clear interval and stop persistent vibrating 
-	if(vibrateInterval) clearInterval(vibrateInterval);
-	navigator.vibrate(0);
-	}
-
-	// Start persistent vibration at given duration and interval
-	var startPeristentVibrate = function startPeristentVibrate(duration, interval) {
-		vibrateInterval = setInterval(function() {
-			startVibrate(duration);
-		}, interval);
+	var stopVibrate = function stopVibrate() { 
+		navigator.vibrate(0);
 	}
 
 	if ('vibrate' in navigator) {
@@ -28,8 +16,9 @@ var vibrate = function vibrate(){
 	   // Not supported
 	   alert("Vibration API not supported!");
 	}
-	$('#vibrate-start').click(function(){startVibrate(1000)});
-	$('#vibrate-persistent').click(function(){startPeristentVibrate(1000, 1500)});
+
+	$('#vibrate-start').click(function(){startVibrate(600)});
+	$('#vibrate-multiple').click(function(){startVibrate([1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000])});
 	$('#vibrate-stop').click(function(){stopVibrate()});
 };
 
